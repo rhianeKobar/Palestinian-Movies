57d7ac8ba6b903be30d07f4fb1d9dec722de2516
rhianekobar <shakira.r.salazar@gmail.com>
2024-04-24 23:00:04 +0100

    query types added

---
+++ Schemas/index.js
@@ -0,0 +1,42 @@
+const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQlSchema,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList
+} = graphql;
+
+const movieData = require("../palestinian_movies_dataset.json");
+const { MovieType, ReleaseDateType} = require("./typeDefs");
+const {GraphQLSchema} = require('graphql/type');
+
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllMovies: {
+      type: new GraphQLList(MovieType),
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return movieData;
+      }
+    },
+    getMovieById: {
+      type: MovieType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args, context) {
+        return movieData.find(movie => movie.id === args.id)
+      }
+    },
+    getMovieByReleaseYear: {
+      type: new GraphQLList(MovieType),
+      args: { year: { type: GraphQLInt } },
+      resolve(parent, args ){
+        return movieData.filter(movie => movie.releaseDate.year === args.year)
+      }
+    },
+  }
+})
+
+module.exports = new GraphQLSchema({ query: RootQuery})
---
+++ Schemas/typeDefs.js
@@ -0,0 +1,93 @@
+const graphql = require("graphql");
+const {GraphQLEnumType} = require('graphql/type');
+const { GraphQLObjectType, GraphQLInt, GraphQLString } = graphql;
+
+const TextType = new GraphQLObjectType({
+  name: "Text",
+  fields: () => ({
+    text: { type: GraphQLString }
+  })
+});
+
+const PlainTextType = new GraphQLObjectType({
+  name: "PlainText",
+  fields: () => ({
+    plainText: { type: GraphQLString }
+  })
+});
+
+const CountryCodeEnum = new GraphQLEnumType({
+  name: "CountryCode",
+  values: {
+    US: { value: 'US' },
+    FR: { value: 'FR' },
+    PS: { value: 'PS' },
+    IL: { value: 'IL' },
+    NL: { value: 'NL' },
+    DE: { value: 'DE' },
+    QA: { value: 'QA' },
+    AE: { value: 'AE' },
+    SG: { value: 'SG' },
+    GB: { value: 'GB' },
+  }
+})
+
+const CountryType = new GraphQLObjectType({
+  name: "Country",
+  fields: () => ({
+    id: { type: CountryCodeEnum },
+    text: { type: GraphQLString }
+  })
+});
+
+const LanguageType = new GraphQLObjectType({
+  name: "Language",
+  fields: () => ({
+    id: { type: GraphQLString }
+  })
+});
+
+const PlotType = new GraphQLObjectType({
+  name: "Plot",
+  fields: () => ({
+    plotText: { type: PlainTextType },
+    language: { type: LanguageType }
+  })
+});
+
+const ReleaseDateType = new GraphQLObjectType({
+  name: "ReleaseDate",
+  fields: () => ({
+    day: { type: GraphQLInt},
+    month: { type: GraphQLInt},
+    year: { type: GraphQLInt},
+    country: { type: CountryType},
+  })
+});
+
+const ImageType = new GraphQLObjectType({
+  name: "Image",
+  fields: () => ({
+    id: { type: GraphQLString},
+    width: { type: GraphQLInt},
+    height: { type: GraphQLInt},
+    url: { type: GraphQLString},
+    caption: { type: PlainTextType},
+  })
+});
+
+const MovieType = new GraphQLObjectType({
+  name: "Movie",
+  fields: () => ({
+    titleText: { type: TextType},
+    id: { type: GraphQLString},
+    primaryImage: { type: ImageType},
+    releaseDate: { type: ReleaseDateType},
+    plot: { type: PlotType},
+  })
+});
+
+module.exports = {
+  MovieType,
+  ReleaseDateType
+};

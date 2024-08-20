ab61c8b5867c61afc84b172223bde637cb08871c
rhianeKobar <shakira.r.salazar@gmail.com>
2024-08-20 12:08:18 +0100

    add API key

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+require("@dotenvx/dotenvx").config();
+
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT ;
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./Schemas/index");
 const cors = require("cors");
+const helmet = require("helmet");
+
+function ValidateApiKey(req, res, next) {
+
+	const apiKey = req.headers["x-api-key"];
+
+	 if (!apiKey) {
+     return res.status(401).json({ error: "API key is missing!" });
+   }
+
+   if (apiKey !== process.env.API_KEY) {
+     return res.status(403).json({ error: "API key is invalid" });
+   }
+
+	next();
+
+}
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.disable("x-powered-by");
+
 let requests = [];
 app.use(
   "/graphql",
-  (req, res, next)=> {
+	ValidateApiKey,
+  (req, res, next) => {
+
     const requestInfo = {
       method: req.method,
       statusCode: res.statusCode,
@@ -33,7 +57,7 @@ app.use(
 
 app.get('/debug/responses', (req, res) => {
   res.json(requests);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

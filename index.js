e42dbcf3c51fddde345534000218f2f54755638b
rhianeKobar <shakira.r.salazar@gmail.com>
2024-08-22 17:52:16 +0100

    add rate limiting

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {rateLimit} = require('express-rate-limit')
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  limit: 30, // Limit each IP to 2 requests per `window` (here, per 1 minute)
+  limit: 30, // Limit each IP to 30 requests per `window` (here, per 1 minute)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
